8315bf96975b801fb89945367de0df15b8727cd6
ogar_m <ogar.m.v@maio.ru>
2025-03-31 10:33:48 +0300

    buses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ public class Main {
         Bus bus = new Bus(0.010);
         bus.refuel(1);
         System.out.println("На " + bus.powerReserve() + " км хватит топлива");
-
+        System.out.println("Едем ещё 50 км: " + bus.run(60));
+        System.out.println("На " + bus.powerReserve() + " км хватит топлива");
+        System.out.println("Едем ещё 60 км: " + bus.run(50));
+        System.out.println("На " + bus.powerReserve() + " км хватит топлива");
     }
 }
