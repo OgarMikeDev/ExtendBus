11200666e30f95746119e5db4aec93104ead159d
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2025-03-31 12:59:09 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,20 @@ public class Main {
     public static void main(String[] args) {
         Bus bus = new Bus(0.300);
         bus.refuel(95);
-        System.out.println("Обычный автобус:");
-        System.out.println("На " + bus.powerReserve() + " км хватит топлива");
+        System.out.println("Автобус:");
+        System.out.println("На " + bus.drivingDistance() + " км хватит топлива :)");
         System.out.println("Едем 310 км: " + bus.run(310));
-        System.out.println("На " + bus.powerReserve() + " км хватит топлива");
+        System.out.println("На " + bus.drivingDistance() + " км хватит топлива :)");
         System.out.println("Едем ещё 5 км: " + bus.run(5));
-        System.out.println("На " + bus.powerReserve() + " км хватит топлива\n");
+        System.out.println("На " + bus.drivingDistance() + " км хватит топлива :)");
 
         ElectricBus electricBus = new ElectricBus(0.300, 9.5);
         electricBus.refuel(95);
-        System.out.println("На " + electricBus.powerReserve() + " км хватит топлива");
+        System.out.println("\nЭлектробус:");
+        System.out.println("На " + electricBus.drivingDistance() + " км хватит топлива :)");
         System.out.println("Едем 310 км: " + electricBus.run(310));
-        System.out.println("На " + electricBus.powerReserve() + " км хватит топлива");
+        System.out.println("На " + electricBus.drivingDistance() + " км хватит топлива :)");
         System.out.println("Едем ещё 5 км: " + electricBus.run(5));
-        System.out.println("На " + electricBus.powerReserve() + " км хватит топлива\n");
+        System.out.println("На " + electricBus.drivingDistance() + " км хватит топлива :)");
     }
 }
